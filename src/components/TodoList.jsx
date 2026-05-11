6844b6bc1674fc6f1742f8841069db592d31eb40
Haryadi Nugraha Putra <h.nugraha.p@gmail.com>
2026-05-11 15:49:45 +0700

    feat: add localStorage

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,9 +1,16 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import TodoInput from './TodoInput.jsx';
 import TodoItem from './TodoItem.jsx';
 
 export default function TodoList() {
-  const [todos, setTodos] = useState([{ text: 'Belajar React', done: false }]);
+  const [todos, setTodos] = useState(() => {
+    const saved = localStorage.getItem('todos');
+    return saved ? JSON.parse(saved) : [];
+  });
+  console.log(localStorage);
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos));
+  }, [todos]);
   return (
     <>
       <TodoInput todos={todos} setTodos={setTodos} />
