eccea6c9d193c4e5869ded4bc6aa3ccb480dba01
Haryadi Nugraha Putra <h.nugraha.p@gmail.com>
2026-05-11 15:22:36 +0700

    chore: seperate TodoToggle component

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,4 +1,5 @@
 import TodoDelete from './TodoDelete';
+import TodoToggle from './TodoToggle';
 
 export default function TodoItem({ todos, setTodos }) {
   return (
@@ -10,36 +11,13 @@ export default function TodoItem({ todos, setTodos }) {
             className='p-4 rounded-2xl border-2 border-[#fe643b] shadow-[0_0_5px_rgba(0,0,0,0.3)] flex justify-between'
           >
             <div className='flex justify-center items-center gap-4'>
-              <button
-                className='cursor-pointer flex justify-center items-center'
-                onClick={() =>
-                  setTodos(
-                    todos.map((todo, indexTodo) => {
-                      if (indexTodo === index) {
-                        return { ...todo, done: !todo.done };
-                      }
-                      return todo;
-                    })
-                  )
-                }
-              >
-                {todo.done ? (
-                  <svg
-                    xmlns='http://www.w3.org/2000/svg'
-                    viewBox='0 0 24 24'
-                    fill='#fe643b'
-                    className='size-8'
-                  >
-                    <path
-                      fillRule='evenodd'
-                      d='M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm13.36-1.814a.75.75 0 1 0-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 0 0-1.06 1.06l2.25 2.25a.75.75 0 0 0 1.14-.094l3.75-5.25Z'
-                      clipRule='evenodd'
-                    />
-                  </svg>
-                ) : (
-                  <div className='size-8 rounded-lg border-2 border-[#fe643b] '></div>
-                )}
-              </button>
+              <TodoToggle
+                todos={todos}
+                setTodos={setTodos}
+                todo={todo}
+                index={index}
+              />
+
               {todo.text}
             </div>
             <TodoDelete todos={todos} setTodos={setTodos} index={index} />
--- src/components/TodoToggle.jsx
+++ src/components/TodoToggle.jsx
@@ -1,3 +1,30 @@
-export default function TodoToggle() {
-  return <></>;
+export default function TodoToggle({ todos, setTodos, todo, index }) {
+  return (
+    <button
+      className={`p-4 cursor-pointer flex justify-center items-center relative rounded-lg border-2 border-[#fe643b]`}
+      onClick={() =>
+        setTodos(
+          todos.map((todo, indexTodo) => {
+            if (indexTodo === index) {
+              return { ...todo, done: !todo.done };
+            }
+            return todo;
+          })
+        )
+      }
+    >
+      <svg
+        xmlns='http://www.w3.org/2000/svg'
+        viewBox='0 0 24 24'
+        fill='#fe643b'
+        className={`size-8 transition-all duration-300 ${todo.done ? 'opacity-100 scale-100' : 'opacity-0 scale-50'} absolute left-0`}
+      >
+        <path
+          fillRule='evenodd'
+          d='M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm13.36-1.814a.75.75 0 1 0-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 0 0-1.06 1.06l2.25 2.25a.75.75 0 0 0 1.14-.094l3.75-5.25Z'
+          clipRule='evenodd'
+        />
+      </svg>
+    </button>
+  );
 }
